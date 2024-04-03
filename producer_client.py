035c6f3c157213a7cd20ae041e07243cb441160e
mutumagitonga <mutumamwiti11@gmail.com>
2024-04-04 00:21:40 +0300

    feat: Add producer client file & configure access to kafka clusters via basic authentication

---
+++ producer_client.py
@@ -0,0 +1,23 @@
+import os
+
+import yfinance as yf
+from confluent_kafka import Producer
+from dotenv import load_dotenv
+
+# Load the API key & API secret from the .env file using load_dotenv() method
+load_dotenv()
+
+# Get API key & secret key
+confluent_api_key = os.environ.get("CONFLUENT_CLUSTER_API_KEY")
+confluent_api_secret_key = os.environ.get("CONFLUENT_CLUSTER_API_SECRET_KEY")
+
+# Initialize Kafka producer...
+# ... as defined in https://docs.confluent.io/kafka-clients/python/current/overview.html
+producer = Producer({
+    'bootstrap.servers': 'pkc-w8nyg.me-central1.gcp.confluent.cloud:9092',
+    'sasl.mechanisms': 'PLAIN',
+    'security.protocol': 'SASL_SSL',
+    'sasl.username': '"{}"'.format(confluent_api_key),
+    'sasl.password': '"{}"'.format(confluent_api_secret_key),
+})
+
