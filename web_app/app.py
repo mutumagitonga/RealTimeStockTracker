c70bd42a8c10b70e524d860416d237a708b213de
mutumagitonga <mutumamwiti11@gmail.com>
2024-04-20 04:23:50 +0300

    feat: Add logic to display only current stock prices

--- web_app/app.py
+++ web_app/app.py
@@ -18,3 +18,35 @@ app.layout = html.Div(
         dcc.Interval(id='interval-comp', interval=3000, n_intervals=0)
     ]
 )
+
+stock_monitor = OrderedDict()  # Declare an ordered dictionary (maintains order items were added)
+
+
+@app.callback(
+    Output('stock-track', 'figure'),
+    [Input('interval-comp', 'interval')]
+)
+def live_graph_update(interval):
+    # Read published messages from Redis list
+    fetched_data = redis_client.lrange('stock_monitor', 0, -1)
+    for msg in fetched_data:
+        stock_monitor[msg['symbol']] = (msg['date'], round(msg['price'], 2))  # Tuple: dates & prices to stock tickers
+
+        # Since it's streaming from known recurring symbols & data, current values are used
+        current_ticker = msg['symbol']
+        current_date = stock_monitor[current_ticker][0]
+        current_price = stock_monitor[current_ticker][1]
+        # sorted_tickers = stock_monitor.keys()  # Sort the keys
+        # sorted_dates = [stock_monitor[symbol][0] for symbol in sorted_tickers]
+        # sorted_prices = [stock_monitor[symbol][1] for symbol in sorted_tickers]  # List of values for current ticker
+
+
+
+        fig = px.scatter(x=current_date, y=current_price,
+                         size="pop", color=, title=str(value),
+                         hover_name="country", log_x=True, size_max=60).update_layout(showlegend=True, title_x=0.5)
+
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
